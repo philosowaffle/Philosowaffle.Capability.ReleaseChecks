160012d9a0dbdc22ec534587a17ef03fba7f23de
Bailey Belvis <baileyb622@gmail.com>
2022-11-17 17:55:38 -0600

    start adding in gh release check stuff

---
+++ src/Philosowaffle.Capability.ReleaseChecks/GitHub/GitHubLatestRelease.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.GitHub.Dto;
+
+record GitHubLatestReleaseResponse
+{
+	public string? Html_Url { get; init; }
+	public string? Tag_Name { get; init; }
+	public DateTime Published_At { get; init; }
+	public string? Body { get; init; }
+}
---
+++ src/Philosowaffle.Capability.ReleaseChecks/Model/LatestReleaseInformation.cs
@@ -0,0 +1,10 @@
+﻿namespace Philosowaffle.Capability.ReleaseChecks.Model;
+
+public struct LatestReleaseInformation 
+{
+	public string? LatestVersion;
+	public DateTime ReleaseDate;
+	public string? ReleaseUrl;
+	public string? Description;
+	public bool IsReleaseNewerThanInstalledVersion;
+}
