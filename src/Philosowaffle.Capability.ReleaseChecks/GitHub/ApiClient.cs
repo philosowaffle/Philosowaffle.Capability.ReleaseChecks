08f6d0ab4a6d2ba6f42fe14417cb2f1cc98f2050
philosowaffle <baileyb622@gmail.com>
2022-11-19 09:26:51 -0600

    [1] init library(#2)
    * [1] init
    
    * init sln config
    
    * start adding in gh release check stuff
    
    * add services and some ioc
    
    * code complete
    
    * add github actions
    
    * fix solution file
    
    * remove unused dependency

---
+++ src/Philosowaffle.Capability.ReleaseChecks/GitHub/ApiClient.cs
@@ -0,0 +1,21 @@
+﻿using Flurl.Http;
+
+namespace Philosowaffle.Capability.ReleaseChecks.GitHub;
+
+internal interface IGitHubApiClient
+{
+	Task<GitHubLatestReleaseResponse> GetLatestReleaseAsync(string organization, string repository);
+}
+
+internal sealed class ApiClient : IGitHubApiClient
+{
+	private const string BASE_URL = "https://api.github.com";
+
+	public Task<GitHubLatestReleaseResponse> GetLatestReleaseAsync(string organization, string repository)
+	{
+		return $"{BASE_URL}/repos/{organization}/{repository}/releases/latest"
+			.WithHeader("Accept", "application/json")
+			.WithHeader("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0")
+			.GetJsonAsync<GitHubLatestReleaseResponse>();
+	}
+}
---
+++ src/Philosowaffle.Capability.ReleaseChecks/GitHub/GitHubLatestRelease.cs
@@ -0,0 +1,9 @@
+﻿namespace Philosowaffle.Capability.ReleaseChecks.GitHub;
+
+internal sealed record GitHubLatestReleaseResponse
+{
+	public string? Html_Url { get; init; }
+	public string? Tag_Name { get; init; }
+	public DateTime Published_At { get; init; }
+	public string? Body { get; init; }
+}
